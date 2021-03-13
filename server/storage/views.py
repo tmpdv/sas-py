9d80dea4d687228594302d79c1fd6e0a2978c0bd
Timofey_Podvorny <Timofey_Podvorny@epam.com>
2021-03-13 14:16:12 +0300

    Upload

--- server/storage/views.py
+++ server/storage/views.py
@@ -1,6 +1,7 @@
 import os
 from django.http import HttpResponse
 from django.conf import settings
+from .forms import UploadFileForm
 
 
 def get_audio(request, link):
@@ -11,3 +12,19 @@ def get_audio(request, link):
 def get_image(request, link):
     audio = open(os.path.join(settings.IMAGE_ROOT, link), "rb").read()
     return HttpResponse(audio, content_type="image/jpeg")
+
+
+def upload_audio(request):
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse("success")
+    else:
+        return HttpResponse("fail")
+
+
+def handle_uploaded_file(f):
+    with open(os.path.join(settings.AUDIO_ROOT, "new_track.mp3"), 'rb') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
